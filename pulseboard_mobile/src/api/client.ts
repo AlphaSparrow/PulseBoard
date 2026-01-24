4f660429dcffa5aa65dfec0a5d28f3e46bdd65e9
amishanainwani123 <nainwaniamisha@gmail.com>
2026-01-24 15:39:54 +0530

    added prefernce page

--- pulseboard_mobile/src/api/client.ts
+++ pulseboard_mobile/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://172.31.10.201:3000", // NOT localhost for mobile
+  baseURL: "http://172.31.10.200:3000", // NOT localhost for mobile
   headers: {
     "Content-Type": "application/json",
   },
